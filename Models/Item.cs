a8108fa1436fd5cc9eca17b7d60f685df250f0ba
jbatu <jorge.batun@tecdesoftware.edu.mx>
2026-05-06 11:07:05 -0600

    Agregar archivos de proyecto.

---
+++ Models/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalogo.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public String Titulo { get; set; }
+        public String Genero { get; set; }
+        public int Ano { get; set; }
+        public String Consola { get; set; }
+        public String Descripcion { get; set; }
+
+    }
+    }
---
+++ wwwroot/js/gamer.js
@@ -0,0 +1,35 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const elementos = document.querySelectorAll(".animar");
+
+    const mostrarElemento = () => {
+        elementos.forEach(elemento => {
+            const posicion = elemento.getBoundingClientRect().top;
+            const alturaPantalla = window.innerHeight;
+
+            if (posicion < alturaPantalla - 80) {
+                elemento.classList.add("visible");
+            }
+        });
+    };
+
+    mostrarElemento();
+    window.addEventListener("scroll", mostrarElemento);
+
+    const cards = document.querySelectorAll(".card-juego");
+
+    cards.forEach(card => {
+        card.addEventListener("mousemove", e => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            card.style.background = `
+                radial-gradient(circle at ${x}px ${y}px, rgba(56,189,248,0.20), rgba(15,23,42,0.95) 45%)
+            `;
+        });
+
+        card.addEventListener("mouseleave", () => {
+            card.style.background = "rgba(15, 23, 42, 0.92)";
+        });
+    });
+});
